6adf23d4fcbd8fa3acb10215be3062e81d09414f
sbasra41 <144752361+sbasra41@users.noreply.github.com>
2023-09-14 05:31:34 +0000

    conversion_2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,16 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// 33abc => NaN
+// true  => 1; false => 0
+
+let isLogedIn = 1
+let booleanLoggedIn = Boolean(isLogedIn)
+console.log(booleanLoggedIn);
